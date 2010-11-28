3f94397c2fc23410d56f04796e41f12bb61ce5c0
Enrique Erne <enrique.erne@mild.ch>
2010-11-28 20:54:01 +0100

    added async Listener, currently disabled sender

--- Source/pd-socket.py
+++ Source/pd-socket.py
@@ -1,14 +1,40 @@
-import socket, threading, os, sys
+"""
+---
+
+name: Pd-Socket
+
+version: 0.2.0
+
+authors:
+- Enrique Erne
+
+license: MIT license
+
+...
+"""
+
+import asyncore, os, socket, sys, threading
 
 class Puredata(threading.Thread):
-	def prepare(self, pd = 'pd', dir = './', file = 'pd-socket.pd', args = '-stderr -nostdpath -rt -send \"pd dsp 1;pd dsp 0;\"'):
+
+	def prepare(self, pd = None, dir = './', file = 'pd-socket.pd', args = '-stderr -nostdpath -rt -send \"pd dsp 1;pd dsp 0;\"'):
 		self.pd = pd
 		self.dir = dir
 		self.file = file
 		# args = -stderr -nostdpath -rt -nogui -path <path> -audiobuf <n> -nostdpath -nogui -send \"pd dsp 1;pd dsp 0;\"
 		# note in -nogui you can't use -send
 		self.args = args
+		if pd != None:
+			self.pd = pd
+		else:
+			if sys.platform == 'linux2':
+				self.pd = 'pd'
+			elif sys.platform == 'darwin':
+				self.pd = '//Applications/Pd-0.42-5.app/Contents/Resources/bin'
+			elif sys.platform == 'win32':
+				self.pd = 'pd\\bin\\pd.exe'
 		return self
+		
 	def run(self):
 		try:
 			os.system('cd %s && ./pd %s %s/%s' %(self.pd, self.args, self.dir, self.file))
@@ -17,18 +43,54 @@ class Puredata(threading.Thread):
 		finally:
 			print 'bye bye Pd'
 
-def PdSocket(PORTOUT = 3005):
-	if sys.platform == 'linux2':
-		PD = 'pd'
-	elif sys.platform == 'darwin':
-		PD = '//Applications/Pd-0.42-5.app/Contents/Resources/bin'
-	elif sys.platform == 'win32':
-		PD = 'pd\\bin\\pd.exe'
-		
-	pd = Puredata()
-	pd.prepare(pd = PD, dir = os.getcwd())
-	pd.start()
+
+class AsyncSocket(threading.Thread):
 	
+	def prepare(self):
+	
+		class Listen(asyncore.dispatcher_with_send):
+
+			def handle_read(self):
+				data = self.recv(8192)
+				#self.send(data)
+				print data
+
+		class Open(asyncore.dispatcher):
+
+			def __init__(self, host, port):
+				asyncore.dispatcher.__init__(self)
+				self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+				self.set_reuse_addr()
+				self.bind((host, port))
+				self.listen(1)
+				print 'do something'
+
+			def handle_accept(self):
+				pair = self.accept()
+				if pair is None:
+					pass
+				else:
+					sock, addr = pair
+					print 'Incoming connection from %s' % repr(addr)
+					Listen(sock)
+		
+		self.listen = Open('localhost', 8080)
+		
+	def run(self):
+		asyncore.loop()
+		
+def init(PORTOUT = 3005):
+	
+	receive = AsyncSocket()
+	receive.prepare()
+	receive.start()
+	
+	pd = Puredata()
+	pd.prepare(dir = os.getcwd())
+	pd.start()
+	print 'done'
+	
+"""
 	while 1:
 		try:
 			pd.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,26 +102,7 @@ def PdSocket(PORTOUT = 3005):
 	
 	pd.socket.send('python->pd Hello Pd!;\n')
 	pd.socket.send('some more...;\n...messages at once;\n')
-
+"""
 if __name__ == '__main__':
-	PdSocket()
+	init()
 
-
-"""
-PORTIN = 3006
-class Listener(threading.Thread):
-	def run(self):
-		r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		r.bind(('localhost', PORTIN))
-		r.listen(2)
-		pd.send('pd pd-py-socket;')
-		client_socket, address = r.accept()
-		# print 'python: got a connection from ', address
-		while 1:
-			data = client_socket.recv(1024)
-			if data:
-				print "python:" , data
-		r.close()
-
-Listener().start()
-"""
